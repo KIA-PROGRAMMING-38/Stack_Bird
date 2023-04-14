96a6c73dd08e83580f7c2d0de49c1c918388f9bf
kcs9608 <120005275+kcs9608@users.noreply.github.com>
2023-04-14 17:16:10 +0900

    Bgm,Sfx 소리 조절

--- Project_Stacky_Bird/Assets/Scripts/Sound/SoundManager.cs
+++ Project_Stacky_Bird/Assets/Scripts/Sound/SoundManager.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 { 
     public AudioSource BirdSound_1, BirdSound_2, BirdSound_3, BirdSound_4;
+
+    [SerializeField] private AudioMixer audioMixer;
+
+    [SerializeField] private AudioSource Bgm;
+
+    [SerializeField] Slider BgmSlider, SfxSlider;
 
     private static SoundManager instance;
 
@@ -30,5 +38,15 @@ public class SoundManager : MonoBehaviour
         else if (instance != this)
             Destroy(gameObject);
         // DontDestroyOnLoad(gameObject); 씬이 추가되면 사용
+    }
+
+    public void SetBgmVolume()
+    {
+        audioMixer.SetFloat("Bgm", Mathf.Log10(BgmSlider.value) * 20);
+    }
+
+    public void SetSfxVolume()
+    {
+        audioMixer.SetFloat("Sfx", Mathf.Log10(SfxSlider.value) * 20);
     }
 }
